0a589f90d41205ac9934d355d16dd4056df314bb
yeahservice <dom.augustin@gmx.at>
2019-09-03 22:42:39 +0200

    Use actual test images instead of splitting from training images

--- main.py
+++ main.py
@@ -4,20 +4,11 @@ import neuralnetwork as nn
 with np.load('data/mnist.npz') as data: 
     training_images = data['training_images']
     training_labels = data['training_labels']
-
-# plt.imshow(training_images[0].reshape(28,28), cmap = 'gray')
-# plt.show()
-
-training_set_size = 500
-
-training_set_images = training_images[:training_set_size]
-training_set_labels = training_labels[:training_set_size]
-
-test_set_images = training_images[training_set_size:]
-test_set_labels = training_labels[training_set_size:]
+    test_images = data['test_images']
+    test_labels = data['test_labels']
 
 layer_sizes = (784, 5, 10)
 
 net = nn.NeuralNetwork(layer_sizes)
-net.print_accuracy(test_set_images, test_set_labels)
-net.calculate_average_cost(test_set_images, test_set_labels)
+net.print_accuracy(test_images, test_labels)
+net.calculate_average_cost(test_images, test_labels)
