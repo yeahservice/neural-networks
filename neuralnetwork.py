fc90922d977a90787abcb02d7899530692d70a84
yeahservice <dom.augustin@gmx.at>
2019-09-11 13:53:45 +0200

    Renamed predict to feedforward and added method signature for training with stochastic gradient descent

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -8,18 +8,23 @@ class NeuralNetwork:
         self.weights = [np.random.standard_normal(s) / np.sqrt(s[1]) for s in weight_shapes]
         self.biases = [np.zeros((s, 1)) for s in layer_sizes[1:]]
 
-    def predict (self, a):
+    def feedforward (self, a):
         for w, b in zip(self.weights, self.biases):
             a = NeuralNetwork.activation(np.matmul(w, a) + b)
-        return a    
+        return a
+
+    def train_sgd(self, training_data, training_labels, epochs, batch_size, learning_rate):
+        """
+        Train the neural network using stochastic gradient decent
+        """
 
     def print_accuracy(self, images, labels):
-        predictions = self.predict(images)
+        predictions = self.feedforward(images)
         num_correct = sum([np.argmax(p) == np.argmax(l) for p, l in zip(predictions, labels)])
         print("{0}/{1} accuracy: {2}%".format(num_correct, len(images), (num_correct / len(images)) * 100))
 
     def calculate_average_cost(self, images, labels):
-        predictions = self.predict(images)
+        predictions = self.feedforward(images)
         average_cost = sum([NeuralNetwork.cost_function(p, l) for p, l in zip(predictions, labels)]) / len(images)
         print("average cost: {0}".format(average_cost))
 
